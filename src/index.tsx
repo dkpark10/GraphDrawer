52d4cc2614cb8decfe8996297f046d17372fe2b9
dkpark10 <dkpark10@naver.com>
2023-03-29 21:33:32 +0900

    fix: vite 실행 되도록 수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,15 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import composeWithDevTools from 'redux-devtools-extension'; // 리덕스 개발자 도구
-import { BrowserRouter } from 'react-router-dom';
 import rootReducer from './redux/index';
 
 const store = createStore(rootReducer, composeWithDevTools);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
