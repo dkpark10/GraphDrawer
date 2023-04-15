e2a6267fafbe6a022d7ce2f4ff3c145bcf44aa5b
dkpark10 <dkpark10@naver.com>
2023-04-15 15:58:21 +0900

    chore: vanilla extract 제거 및 tailwind 설치

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { Provider } from 'react-redux';
+import App from './App';
 import { store } from './store';
 
 ReactDOM.render(
@@ -10,5 +10,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
