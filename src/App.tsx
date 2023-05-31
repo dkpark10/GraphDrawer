0b8f6860be7e4cc5827b9f3c828d3dc825bbca74
dkpark10 <dkpark10@naver.com>
2023-05-31 13:47:16 +0900

    feat: 화살표 마커 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-use-before-define */
@@ -26,13 +28,148 @@
 //   );
 // }
 
+// import React, { useEffect, useRef, useState } from 'react';
+// import * as d3 from 'd3';
+// import type { SimulationNodeDatum, SimulationLinkDatum } from 'd3-force';
+
+// interface Node {
+//   id?: string;
+//   group?: number;
+// }
+
+// interface Edge {
+//   source?: string;
+//   target?: string;
+//   value?: number;
+// }
+
+// interface AppProps {
+//   nodeList: {
+//     nodes: Array<Node & SimulationNodeDatum>;
+//     links: Array<Edge & SimulationLinkDatum<SimulationNodeDatum>>;
+//   };
+// }
+
+// export default function App({ nodeList }: AppProps) {
+//   const svgRef = useRef<SVGSVGElement | null>(null);
+
+//   const [mounted, setMounted] = useState(false);
+
+//   useEffect(() => {
+//     if (!svgRef.current || nodeList.nodes.length <= 0) {
+//       return;
+//     }
+
+//     d3.forceSimulation(nodeList.nodes as SimulationNodeDatum[])
+//       .force(
+//         'link',
+//         d3
+//           .forceLink(nodeList.links)
+//           .id((d: any) => d.id)
+//           .distance(500),
+//       )
+//       .force('charge', d3.forceManyBody().strength(-100))
+//       .force('x', d3.forceX(300))
+//       .force('y', d3.forceY(300))
+//       .on('tick', () => {
+//         setMounted(true);
+//       });
+//   }, [nodeList.nodes, nodeList.links]);
+
+//   return (
+//     <main className="flex justify-center items-center	 border border-red-500">
+//       <svg width="600" height="600" viewBox="0 0 600 600" ref={svgRef}>
+//         {mounted &&
+//           nodeList.links.map(({ source, target }, idx) => {
+//             return (
+//               <g key={idx}>
+//                 <defs>
+//                   <marker
+//                     id="arrow"
+//                     viewBox="0 0 10 10"
+//                     refX="24"
+//                     refY="5"
+//                     markerWidth="8"
+//                     markerHeight="8"
+//                     orient="auto-start-reverse"
+//                   >
+//                     <path d="M 0 0 L 10 5 L 0 10 z" fill="#020617" />
+//                   </marker>
+//                 </defs>
+//                 <path
+//                   d={`M ${(source as SimulationNodeDatum).x} ${(source as SimulationNodeDatum).y} L ${
+//                     (target as SimulationNodeDatum).x
+//                   } ${(target as SimulationNodeDatum).y}`}
+//                   strokeWidth="2"
+//                   stroke="yellow"
+//                   markerEnd="url(#arrow)"
+//                 />
+//                 {/* <text
+//                   className="pointer-events-none"
+//                   y={22}
+//                   x={22}
+//                   dx=".3em"
+//                   dy=".9em"
+//                   fontSize="14"
+//                   fill="pink"
+//                   textAnchor="right"
+//                 >
+//                   {idx}
+//                 </text> */}
+//               </g>
+//             );
+//           })}
+//         {mounted &&
+//           nodeList?.nodes.map((node, idx) => {
+//             console.log(node.y, node.x);
+//             return (
+//               <g className="cursor-pointer" key={idx}>
+//                 <circle cy={node.y} cx={node.x} r="22" fill="red" stroke="blue" strokeWidth="2.5" />
+//                 {/* <text
+//                   y={node.y}
+//                   x={node.x}
+//                   className="pointer-events-none"
+//                   dy=".35em"
+//                   fontSize="15"
+//                   fill="white"
+//                   textAnchor="middle"
+//                 >
+//                   {node.id}
+//                 </text> */}
+//               </g>
+//             );
+//           })}
+//       </svg>
+//     </main>
+//   );
+// }
+
+// --------------------------------------------------------------------------------------------------------------------------------
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-import type { SimulationNodeDatum } from 'd3-force';
+import type { SimulationNodeDatum, SimulationLinkDatum } from 'd3-force';
 import type { Selection } from 'd3-selection';
 import { miserbles } from '@/__mock__';
 
-export default function App() {
+interface Node {
+  id?: string;
+  group?: number;
+}
+
+interface Edge {
+  source?: string;
+  target?: string;
+  value?: number;
+}
+
+interface AppProps {
+  nodeList: {
+    nodes: Array<Node & SimulationNodeDatum>;
+    links: Array<Edge & SimulationLinkDatum<SimulationNodeDatum>>;
+  };
+}
+
+export default function App({ nodeList }: AppProps) {
   const svgRef = useRef<SVGSVGElement | null>(null);
 
   useEffect(() => {
@@ -40,24 +177,37 @@ export default function App() {
       return;
     }
 
-    function dragstarted(event: any) {
+    const dragStarted = (event: any) => {
       if (!event.active) simulation.alphaTarget(0.3).restart();
       event.subject.fx = event.subject.x;
       event.subject.fy = event.subject.y;
-    }
+    };
 
-    function dragged(event: any) {
+    const dragged = (event: any) => {
       event.subject.fx = event.x;
       event.subject.fy = event.y;
-    }
+    };
 
-    function dragended(event: any) {
+    const dragEnded = (event: any) => {
       if (!event.active) simulation.alphaTarget(0);
       event.subject.fx = null;
       event.subject.fy = null;
-    }
+    };
 
     const svg = d3.select(svgRef.current).attr('style', 'border: 1px solid red;');
+    svg
+      .append('defs')
+      .append('marker')
+      .attr('id', 'arrow')
+      .attr('viewBox', '0 0 10 10')
+      .attr('refX', '24')
+      .attr('refY', '5')
+      .attr('markerWidth', '8')
+      .attr('markerHeight', '8')
+      .attr('orient', 'auto-start-reverse');
+
+    const marker = d3.select('#arrow');
+    marker.append('path').attr('d', 'M 0 0 L 10 5 L 0 10 z').attr('fill', '#020617');
 
     const link = svg
       .append('g')
@@ -67,20 +217,22 @@ export default function App() {
       .attr('stroke-linecap', 'round')
       .selectAll('line')
       .data(miserbles.links)
-      .join('line');
+      .join('line')
+      .attr('marker-end', 'url(#arrow)');
 
     const node = svg
       .append('g')
-      .attr('fill', 'red')
-      .attr('stroke', '#fff')
+      .attr('fill', '#060724')
+      .attr('class', 'cursor-pointer')
+      .attr('stroke', '#060724')
       .attr('stroke-opacity', 1)
-      .attr('stroke-width', 1.5)
+      .attr('stroke-width', 2.5)
       .selectAll('circle')
       .data(miserbles.nodes)
       .join('circle')
-      .attr('r', 10)
+      .attr('r', 18)
       .call(
-        d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended) as (
+        d3.drag().on('start', dragStarted).on('drag', dragged).on('end', dragEnded) as (
           selection: Selection<any | SVGCircleElement, { id: string; group: number }, SVGGElement, unknown>,
         ) => void,
       );
@@ -96,19 +248,19 @@ export default function App() {
     };
 
     const simulation = d3
-      .forceSimulation(miserbles.nodes as SimulationNodeDatum[])
+      .forceSimulation(nodeList.nodes as SimulationNodeDatum[])
       .force(
         'link',
         d3
-          .forceLink(miserbles.links)
+          .forceLink(nodeList.links)
           .id((d: any) => d.id)
-          .distance(50),
+          .distance(100),
       )
       .force('charge', d3.forceManyBody().strength(-100))
       .force('x', d3.forceX(300))
       .force('y', d3.forceY(300))
       .on('tick', ticked);
-  }, []);
+  }, [nodeList?.nodes, nodeList?.links]);
 
   return (
     <main className="flex justify-center items-center	 border border-red-500">
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import './styles/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { miserbles } from '@/__mock__';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App nodeList={miserbles} />
   </React.StrictMode>,
   document.getElementById('root'),
 );
