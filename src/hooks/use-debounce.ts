0f3f82de838f95a49fc3cc81a752f8ed28eb9ec1
dkpark10 <dkpark10@naver.com>
2024-03-22 08:57:57 +0900

    feat: 디바운스 훅으로 변경

---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+interface DebounceParam<T> {
+  value: T;
+  delay: number;
+}
+
+export const useDebounce = <T>({ value, delay }: DebounceParam<T>) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
