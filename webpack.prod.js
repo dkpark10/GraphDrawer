c21b74bd1d45d9e9ce8408d65fd467cc9bc4b57c
dkpark10 <dkpark10@naver.com>
2024-05-12 12:37:07 +0900

    chore: 빌드 경로명 변경

--- webpack.prod.js
+++ webpack.prod.js
@@ -28,7 +28,7 @@ const isLocalBuild = process.env.LOCAL_BUILD === 'true';
 module.exports = merge(webpackCommonConfig, {
   mode: 'production',
   output: {
-    path: path.resolve('build/'),
+    path: path.resolve('dist/'),
     filename: 'app.[chunkhash].js',
     // prefix 개념 번들 파일 앞에 요청할 주소 ec2 url 이나 s3
     publicPath: isLocalBuild ? './' : '/graphpainter/',
